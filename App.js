7f2aefe43ccc5502825b451f38550c570d59495a
Juan Diego <silgajuandiego@gmail.com>
2018-11-04 08:17:08 -0500

    [Add] Adicionando la funcionalidad para el play/pausa

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import Home from './src/screens/containers/home'
 import Header from './src/sections/components/header'
 import SuggestionList from './src/videos/containers/suggestion-list'
 import API from './utils/api'
 import CategoryList from './src/videos/containers/category-list'
+import Player from './src/player/containers/player'
 
 // console.disableYellowBox = true // para deshabilitar las advertencias en el localhos:8081/debugger-ui
 
@@ -32,6 +33,7 @@ export default class App extends Component {
     return (
       <Home>
         <Header />
+        <Player />
         <Text>Buscador</Text>
         <Text>Categorías</Text>
         <CategoryList
